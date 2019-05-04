5e4b2e05ed23cfd7e5c4288336db0d9fbba67957
Ramin <ramind123@yahoo.co.uk>
2019-05-04 10:54:20 -0700

    Adding skeleton

--- CReferee.cpp
+++ CReferee.cpp
@@ -1,1 +1,14 @@
 #include "CReferee.hpp"
+
+CReferee::CReferee() :
+id("R000"),
+firstname("None"),
+lastname("None") {}
+
+CReferee::CReferee(std::string const& id_, std::string const& first_, std::string const& last_, RefereeGrade const& grade_) :
+id(id_),
+firstname(first_),
+lastname(last_),
+grade(grade_) {}
+
+CReferee::~CReferee() {}
--- CReferee.hpp
+++ CReferee.hpp
@@ -1,12 +1,20 @@
 #ifndef _CREFEREE_HPP_
 #define _CREFEREE_HPP_
+#include <string>
+
+enum RefereeGrade {UNKNOWN, CLUB, STATE, NATIONAL, FIFA};
 
 class CReferee
 {
 public:
-    /* data */
+    CReferee();
+    CReferee(std::string const&, std::string const&, std::string const&, RefereeGrade const&);
+    ~CReferee();
 private:
-    /* data*/
+    std::string id;
+    std::string firstname;
+    std::string lastname;
+    RefereeGrade grade;
 };
 
 #endif
